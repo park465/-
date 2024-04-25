952dd095291e168079f6ab9ee50bb6282f6232d5
park465 <163487259+park465@users.noreply.github.com>
2024-04-25 20:17:33 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,12 @@
+
+for o in range(1,10):
+    for i in range(2,6):
+        print(f'{i} x {o} = {i*o:2d}\t', end='')
+    print()
+
+print()
+
+for o in range(1,10):
+    for i in range(6,10):
+        print(f'{i} x {o} = {i*o:2d}\t', end='')
+    print()
