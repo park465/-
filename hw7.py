375c2d7234effe6e2649fdd59525e4283acb9f4f
park465 <163487259+park465@users.noreply.github.com>
2024-05-03 13:42:23 +0900

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,18 @@
+shopping_bag={}
+print('[구입]')
+while True:
+    item=input('상품명? ')
+    if item=='':
+        break
+    num=int(input('수량은? '))
+    shopping_bag[item]=num
+    print(f'장바구니에 {item} {num}개가 담겼습니다.')
+    print()
+print(f'\n>>> 장바구니 보기: {shopping_bag}\n')
+
+print('[검색]')
+s=input('장바구니에서 확인하고자 하는 상품은? ')
+if s in shopping_bag:
+    print(f'{s}은(는) {num}개 담겨 있습니다.')
+else:
+    print(f'{s}은(는) 담겨 있지 않습니다.')
