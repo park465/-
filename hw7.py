35ab45fa39fc48d34ad64751f918c53450945304
park465 <163487259+park465@users.noreply.github.com>
2024-05-10 11:04:09 +0900

    hw7.py

--- hw7.py
+++ hw7.py
@@ -13,6 +13,6 @@ print(f'\n>>> 장바구니 보기: {shopping_bag}\n')
 print('[검색]')
 s=input('장바구니에서 확인하고자 하는 상품은? ')
 if s in shopping_bag:
-    print(f'{s}은(는) {num}개 담겨 있습니다.')
+    print(f'{s}은(는) {shopping_bag[s]}개 담겨 있습니다.')
 else:
     print(f'{s}은(는) 담겨 있지 않습니다.')
